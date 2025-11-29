bbba87adaf9791117defb555ed112f54c43d723a
Akash Madhav . Y <akashmadhav@gmail.com>
2025-11-29 03:48:40 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,14 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBqKQRg5Hn6GAzUhEVwPTlM-JwKOZ60y0U",
-  authDomain: "medireportai-2c253.firebaseapp.com",
-  databaseURL: "https://medireportai-2c253-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "medireportai-2c253",
-  storageBucket: "medireportai-2c253.firebasestorage.app",
-  messagingSenderId: "261763392897",
-  appId: "1:261763392897:web:5029020882f6c639b13480",
-  measurementId: "G-T8H52Z202K"
+  apiKey: "AIzaSyDx9mhWylbNwt9cCB2FiKrvWU7tTD-5-z8",
+  authDomain: "medir-a6f0a.firebaseapp.com",
+  projectId: "medir-a6f0a",
+  storageBucket: "medir-a6f0a.firebasestorage.app",
+  messagingSenderId: "153917471576",
+  appId: "1:153917471576:web:4801742c7163e3ec46032d",
+  measurementId: "G-WFPWZLW9R8",
+  databaseURL: "https://medir-a6f0a-default-rtdb.firebaseio.com"
 };
 
 // Initialize Firebase
